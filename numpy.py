53886c10bb96472c528c8c5b22f10d7c95bfca8b
maiphuctram <maithiphuctram@gmail.com>
2020-12-26 22:16:38 +0700

    first commit

---
+++ numpy.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 20:23:29 2020
+
+@author: Laptop Masstel
+"""
+
+import numpy as np
+a = np.array([(1, 2, 3),(4,5,6),(0,5,3)])
+print(a) 
+b= np.array([(0,4,6),(4,8,9),(0,1,7)])
+print(b)
+c=np.add(a,b)
+print(c)
+d=np.dot(a,b)
+print(d)
+n=np.transpose(a)
+print(n)
+
+import pandas as pd
+n=pd.read_csv('C:/Users/Laptop Masstel/Downloads/Bảng tính chưa có tiêu đề - Trang tính1.csv')
+print(n.head(10))
+
+
+
+
+import matplotlib.pyplot as xyz
+xyz.bar([0.24,1.20,2.24,3.20,4.22],[9,7,8,11,10],
+label="dealine",width=.5)
+xyz.bar([.65,1.65,2.55,3.85,4.85],[9,7,5,6,5],label="ngủ",width=.5)
+xyz.legend()
+xyz.xlabel('ngày')
+xyz.ylabel('giờ')
+xyz.title('biểu đồ')
+xyz.show()
+            
